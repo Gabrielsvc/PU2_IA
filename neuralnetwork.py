d8070a4b21b5fc3fe36aac685221625024562432
Gabrielsvc <mrgabral@gmail.com>
2018-05-12 18:00:55 -0300

    Exemplo e definicao dos parametros

---
+++ neuralnetwork.py
@@ -0,0 +1,29 @@
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt 
+
+#Entradas
+'''
+Serie
+Filme
+Dia da semana ou fim de semana
+Criancas ou nao
+Ferias ou nao
+Notas para cada genero
+'''
+#Parametros
+'''
+Numero de camadas ocultas = 1
+Numero de saidas de cada camada = 
+Funcao de ativacao = 
+Batch ou online = 
+Numero de iteracoes de treinamento =
+Criterio de parada = 
+
+'''
+def sum_f():
+	for v in xrange(0,num_entradas):
+
+
+if __name__ == '__main__':
+	main()
